797d7c0ae22c65368361c0049bb1f85061d54438
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-01-12 09:28:09 +0100

    Initial commit.

---
+++ src/test/java/no/ntnu/idata/shiporganizer/shiporganizerservice/ShipOrganizerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata.shiporganizer.shiporganizerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipOrganizerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
