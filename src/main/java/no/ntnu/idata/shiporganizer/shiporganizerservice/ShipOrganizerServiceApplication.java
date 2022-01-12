6e8286cd1e4acd4791d700797c378ef6d933e4d1
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-01-12 09:28:09 +0100

    Initial commit.

---
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/ShipOrganizerServiceApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata.shiporganizer.shiporganizerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipOrganizerServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShipOrganizerServiceApplication.class, args);
+  }
+
+}
