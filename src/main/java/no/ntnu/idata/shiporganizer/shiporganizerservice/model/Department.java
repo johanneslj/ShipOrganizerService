78ecab81f87460e004b9e57a9d765bf3786a97ed
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-02-04 14:00:21 +0100

    Worked on user and department functionality.

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/model/Department.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/model/Department.java
@@ -2,8 +2,6 @@ package no.ntnu.idata.shiporganizer.shiporganizerservice.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,14 +10,53 @@ import javax.persistence.Table;
  * Keeps track of department name and the rights value belonging to the department.
  */
 @Entity
-@Table(name = "department")
+@Table(name = "Department")
 public class Department {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "id")
-  private Long id;
+  @Column(name = "PK_DepartmentID")
+  private int id;
 
-  @Column(name = "name")
+  @Column(name = "DepartmentName")
   private String name;
+
+  @Column(name = "Rights")
+  private int rights;
+
+  public Department() {
+  }
+
+  public Department(String name) {
+    this.name = name;
+  }
+
+  public Department(int id, String name, int rights) {
+    this.id = id;
+    this.name = name;
+    this.rights = rights;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getRights() {
+    return rights;
+  }
+
+  public void setRights(int rights) {
+    this.rights = rights;
+  }
 }
