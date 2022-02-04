702ea8816e3d87fceb178884477918daa0fe744d
Simon Duggal <simondu@stud.ntnu.no>
2022-02-04 14:20:30 +0100

    Added comments

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductContoller.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductContoller.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+/**
+ * The type Product contoller.
+ */
 @RestController
 @RequestMapping(value ="/product")
 @Transactional
@@ -20,21 +23,43 @@ public class ProductContoller {
 
 	private ProductService productService;
 
+	/**
+	 * Instantiates a new Product contoller.
+	 *
+	 * @param productService the product service
+	 */
 	ProductContoller(ProductService productService) {
 		this.productService = productService;
 	}
 
+	/**
+	 * Gets inventory.
+	 *
+	 * @return the inventory
+	 */
 	@GetMapping(path = "/inventory")
 	@ResponseBody
 	public List<Product> getInventory() {
 		return productService.getProductInventory("");
 	}
+
+	/**
+	 * Gets preferred inventory.
+	 *
+	 * @return the preferred inventory
+	 */
 	@GetMapping(path = "/PreferredInventory")
 	@ResponseBody
 	public List<Product> getPreferredInventory() {
 		return productService.getProductPreferredInventory("");
 	}
 
+	/**
+	 * Set new stock
+	 *
+	 * @param requestBody the request body
+	 * @return 200 OK or 204 No content
+	 */
 	@PostMapping(path = "/setNewStock")
 	public ResponseEntity setNewStock(@RequestBody String requestBody){
 		if(productService.setNewStock(requestBody).equals("Success")){
