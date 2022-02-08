12bc01f417f02a4046f6e7d3c62ce366a62f8bc9
Hans <hansal@ntnu.no>
2022-02-08 09:42:53 +0100

    Added documentation

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ReportController.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ReportController.java
@@ -1,9 +1,10 @@
 package no.ntnu.idata.shiporganizer.shiporganizerservice.controller;
 
 import java.util.List;
+import java.util.Map;
 import javax.transaction.Transactional;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Report;
-import no.ntnu.idata.shiporganizer.shiporganizerservice.repository.ReportRepository;
+import no.ntnu.idata.shiporganizer.shiporganizerservice.service.ReportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Transactional
 public class ReportController {
 
-    private final ReportRepository reportRepository;
+    private final ReportService reportService;
 
-    ReportController(ReportRepository reportRepository) {
-        this.reportRepository = reportRepository;
+    ReportController(ReportService reportService) {
+        this.reportService = reportService;
     }
 
     @GetMapping("/all-reports")
-    public ResponseEntity<List<String>> getAllReports() {
-        return ResponseEntity.ok(reportRepository.getMapMarkers());
+    public ResponseEntity<Map<String, List<Report>>> getAllReports() {
+        return ResponseEntity.ok(reportService.getMapMarkers());
     }
 
 
