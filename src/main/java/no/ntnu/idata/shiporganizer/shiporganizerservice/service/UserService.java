c85754ac9f68dbc66fe216c85d0d0a0a125e0ac4
Hans <hansal@ntnu.no>
2022-02-08 14:06:44 +0100

    Small fixes in report controller, and started working on email services

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/UserService.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/UserService.java
@@ -9,47 +9,47 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-  final private UserRepository userRepository;
+    final private UserRepository userRepository;
 
-  public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
-
-  public List<User> getAllUsers() {
-    return userRepository.findAll();
-  }
-
-  public void addNewUserWithDepartments(User user, List<Department> departments) {
-    userRepository.addUser(user.getEmail(), user.getPassword(), user.getFullname());
-
-    StringBuilder departmentsString = new StringBuilder();
-    for (Department department : departments) {
-      departmentsString.append(department.getName());
-      departmentsString.append(",");
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    userRepository.updateUserDepartment(user.getEmail(), departmentsString.toString());
-  }
-
-  public void deleteUser(String username) {
-    userRepository.deleteUser(username);
-  }
-
-  /**
-   * Get all departments user is a member of.
-   *
-   * @param user User to get departments of.
-   * @return List if departments the user is a member of.
-   */
-  List<Department> getDepartments(User user) {
-    List<Department> departments = new ArrayList<Department>();
-    List<String> userDepartments = userRepository.getUserDepartments(user.getEmail());
-
-    for (String userDepartment : userDepartments) {
-      // TODO Handle string and add departments.
-
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
-    return departments;
-  }
+    public void addNewUserWithDepartments(User user, List<Department> departments) {
+        userRepository.addUser(user.getEmail(), user.getPassword(), user.getFullname());
+
+        StringBuilder departmentsString = new StringBuilder();
+        for(Department department : departments) {
+            departmentsString.append(department.getName());
+            departmentsString.append(",");
+        }
+
+        userRepository.updateUserDepartment(user.getEmail(), departmentsString.toString());
+    }
+
+    public void deleteUser(String username) {
+        userRepository.deleteUser(username);
+    }
+
+    /**
+     * Get all departments user is a member of.
+     *
+     * @param user User to get departments of.
+     * @return List if departments the user is a member of.
+     */
+    List<Department> getDepartments(User user) {
+        List<Department> departments = new ArrayList<Department>();
+        List<String> userDepartments = userRepository.getUserDepartments(user.getEmail());
+
+        for(String userDepartment : userDepartments) {
+            // TODO Handle string and add departments.
+
+        }
+
+        return departments;
+    }
 }
