98c99ed2a4d4669f970f32e110c67733b5e41bb2
Simon Duggal <simondu@stud.ntnu.no>
2022-02-11 15:09:02 +0100

    Added method for pending and confirmed orders

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/OrderService.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/OrderService.java
@@ -1,18 +1,23 @@
 package no.ntnu.idata.shiporganizer.shiporganizerservice.service;
 
+import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Orders;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The type Order service. The link between the Controller and the repository classes
  */
+@Service
 public class OrderService {
 
 	/**
 	 * The Order repository.
 	 */
 	@Autowired
-	OrderRepository orderRepository;
+	private final OrderRepository orderRepository;
 
 	/**
 	 * Instantiates a new Order service.
@@ -21,6 +26,38 @@ public class OrderService {
 	 */
 	public OrderService(OrderRepository orderRepository) {
 		this.orderRepository = orderRepository;
+	}
+
+	/**
+	 * Get pending orders list.
+	 *
+	 * @param department the department
+	 * @return the list
+	 */
+	public List<Orders> getPendingOrders(String department){
+		List<Orders> pendingOrders = new ArrayList<>();
+		List<String> bits = orderRepository.getPendingOrders(department);
+		for (String ting : bits) {
+			String[] data = ting.split(",");
+			pendingOrders.add(new Orders(data[0],data[1]));
+		}
+		return pendingOrders;
+	}
+
+	/**
+	 * Get confirmed orders list.
+	 *
+	 * @param department the department
+	 * @return the list
+	 */
+	public List<Orders> getConfirmedOrders(String department){
+		List<Orders> confirmedOrders = new ArrayList<>();
+		List<String> bits = orderRepository.getConfirmedOrders(department);
+		for (String ting : bits) {
+			String[] data = ting.split(",");
+			confirmedOrders.add(new Orders(data[0],data[1]));
+		}
+		return confirmedOrders;
 	}
 
 }
