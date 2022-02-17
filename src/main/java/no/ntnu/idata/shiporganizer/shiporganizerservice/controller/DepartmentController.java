2fd8353260917dece747b4aa542bf4a01feff56c
Simon Duggal <simondu@stud.ntnu.no>
2022-02-17 09:36:59 +0100

    Created Department controller
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

---
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/DepartmentController.java
@@ -0,0 +1,44 @@
+package no.ntnu.idata.shiporganizer.shiporganizerservice.controller;
+
+import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Department;
+import no.ntnu.idata.shiporganizer.shiporganizerservice.repository.DepartmentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * The type Department controller.
+ */
+@Controller
+@RequestMapping(value ="/department")
+@Transactional
+public class DepartmentController {
+
+	/**
+	 * The Department repository.
+	 */
+	DepartmentRepository departmentRepository;
+
+	/**
+	 * Instantiates a new Department controller.
+	 *
+	 * @param DepartmentRepository the department repository
+	 */
+	public DepartmentController(DepartmentRepository DepartmentRepository) {
+		departmentRepository = DepartmentRepository;
+	}
+
+	/**
+	 * Gets departments.
+	 *
+	 * @return the departments
+	 */
+	@GetMapping(path = "/all")
+	public List<Department> getDepartments() {
+		return departmentRepository.findAll();
+	}
+
+}
