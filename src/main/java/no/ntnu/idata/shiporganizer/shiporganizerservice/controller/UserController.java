00d3ed00aa0ad16df312bfb520a879b1835a7304
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-02-23 14:13:50 +0100

    Should now be able to receive verification code by email and use it to set a new password.

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/UserController.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/UserController.java
@@ -7,12 +7,14 @@ import no.ntnu.idata.shiporganizer.shiporganizerservice.model.User;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.service.UserService;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -81,6 +83,50 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 
     } catch (JSONException e) {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
+  /**
+   * Sets a new password for the user using the received verification code.
+   *
+   * @param http HTTP entity.
+   * @return Response Entity.
+   */
+  @PostMapping("/set-password")
+  public ResponseEntity<String> setNewPassword(HttpEntity<String> http) {
+    try {
+      JSONObject json = new JSONObject(http.getBody());
+      String verificationCode = json.getString("code");
+      String email = json.getString("email");
+      String password = json.getString("password");
+
+      boolean isSuccess =
+          userService.setNewPasswordWithVerificationCode(email, verificationCode, password);
+
+      if (isSuccess) {
+        return ResponseEntity.ok("Password changed.");
+      } else {
+        return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+      }
+    } catch (JSONException e) {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
+  /**
+   * Checks if verification code is valid.
+   *
+   * @param email User's email.
+   * @param code  User's received verification code.
+   * @return ResponseEntity 200 OK if valid.
+   */
+  @GetMapping("/check-valid-verification-code")
+  public ResponseEntity<String> checkValidVerificationCode(@Param("email") String email,
+                                                           @Param("code") String code) {
+    if (userService.checkValidVerificationCode(email, code)) {
+      return ResponseEntity.ok("Verification code valid.");
+    } else {
       return ResponseEntity.badRequest().build();
     }
   }
