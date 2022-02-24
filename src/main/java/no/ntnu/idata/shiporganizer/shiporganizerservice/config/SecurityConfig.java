7217025c5f265cb90d3e1439619bab9f430c1534
Simon Duggal <simondu@stud.ntnu.no>
2022-02-24 14:30:58 +0100

    added /actuator/health path to be permitted without token
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/config/SecurityConfig.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/config/SecurityConfig.java
@@ -18,50 +18,51 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final PasswordEncoder passwordEncoder;
-  private final UserPrincipalService userPrincipalService;
-  private final UserService userService;
-  private final JWTProperties jwtProperties;
+	private final PasswordEncoder passwordEncoder;
+	private final UserPrincipalService userPrincipalService;
+	private final UserService userService;
+	private final JWTProperties jwtProperties;
 
-  public SecurityConfig(PasswordEncoder passwordEncoder,
-                        UserPrincipalService userPrincipalService,
-                        UserService userService,
-                        JWTProperties jwtProperties) {
-    this.passwordEncoder = passwordEncoder;
-    this.userPrincipalService = userPrincipalService;
-    this.userService = userService;
-    this.jwtProperties = jwtProperties;
-  }
+	public SecurityConfig(PasswordEncoder passwordEncoder,
+						  UserPrincipalService userPrincipalService,
+						  UserService userService,
+						  JWTProperties jwtProperties) {
+		this.passwordEncoder = passwordEncoder;
+		this.userPrincipalService = userPrincipalService;
+		this.userService = userService;
+		this.jwtProperties = jwtProperties;
+	}
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        .csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService, jwtProperties))
-        .authorizeRequests()
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.addFilter(new JwtAuthenticationFilter(authenticationManager(), userService, jwtProperties))
+				.authorizeRequests()
 
-        // Permit login and registration
-        .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
-        .antMatchers(HttpMethod.POST, "/auth/register").permitAll()
-        .antMatchers(HttpMethod.DELETE, "/api/user/delete-user").hasAnyRole("USER", "ADMIN")
-        .antMatchers(HttpMethod.GET, "/api/user/all-users").hasRole("ADMIN")
-        .anyRequest().authenticated()
-        .and().httpBasic().disable();
-  }
+				// Permit login and registration
+				.antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth/register").permitAll()
+				.antMatchers(HttpMethod.DELETE, "/api/user/delete-user").hasAnyRole("USER", "ADMIN")
+				.antMatchers(HttpMethod.GET, "/api/user/all-users").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and().httpBasic().disable();
+	}
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder authBuilder) {
-    authBuilder.authenticationProvider(daoAuthenticationProvider());
-  }
+	@Override
+	protected void configure(AuthenticationManagerBuilder authBuilder) {
+		authBuilder.authenticationProvider(daoAuthenticationProvider());
+	}
 
-  @Bean
-  DaoAuthenticationProvider daoAuthenticationProvider() {
-    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-    daoAuthenticationProvider.setUserDetailsService(userPrincipalService);
+	@Bean
+	DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+		daoAuthenticationProvider.setUserDetailsService(userPrincipalService);
 
-    return daoAuthenticationProvider;
-  }
+		return daoAuthenticationProvider;
+	}
 }
