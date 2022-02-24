d29e0db01e3a2fcce12d198677a63e79c7ff8fd9
Simon Duggal <simondu@stud.ntnu.no>
2022-02-24 14:30:13 +0100

    Removed unused imports
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/AuthController.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/AuthController.java
@@ -1,7 +1,6 @@
 package no.ntnu.idata.shiporganizer.shiporganizerservice.controller;
 
 import com.auth0.jwt.JWT;
-import com.microsoft.sqlserver.jdbc.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -13,13 +12,10 @@ import no.ntnu.idata.shiporganizer.shiporganizerservice.service.UserService;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductContoller.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductContoller.java
@@ -82,4 +82,6 @@ public class ProductContoller {
 		return ResponseEntity.noContent().build();
 	}
 
+
+
 }
