0140a4e853d89797937a29a884085d7b1613aa03
Hans <hansal@ntnu.no>
2022-02-28 09:41:08 +0100

    Fixed typos

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/repository/ReportRepository.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/repository/ReportRepository.java
@@ -4,14 +4,17 @@ import java.util.List;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 
-    @Query(value = "EXEC SelectAll @CallTime = 'Map', @Department = 'Deck', @Username = '', @ProductName = '';", nativeQuery = true)
+    @Query(value = "EXEC SelectAll @CallTime = 'Map', @Department = '', @Username = '', @ProductName = '';", nativeQuery = true)
     List<String> getMapMarkers();
 
-    @Query(value = "EXEC SelectAll @CallTime = 'Map', @Department = 'Deck', @Username = '';", nativeQuery = true)
-    List<String> getMapMarkersOnName(String name);
+    @Transactional
+    @Query(value = "EXEC SelectAll @CallTime = 'Productmap', @Department = '', @Username = '', @ProductName = :productName ;", nativeQuery = true)
+    List<String> getMapMarkersOnName(@Param(value = "productName") String productName);
 }
