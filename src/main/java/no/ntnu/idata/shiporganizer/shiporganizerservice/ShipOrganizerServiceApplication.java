f6c008d9f40dfc855ebcd26c3e7218c29acc48d4
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-03-02 14:58:06 +0100

    Upgraded to Java 17, and some maven dependencies.

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/ShipOrganizerServiceApplication.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/ShipOrganizerServiceApplication.java
@@ -1,17 +1,10 @@
 package no.ntnu.idata.shiporganizer.shiporganizerservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ShipOrganizerServiceApplication {
-
-  @Autowired
-  private JavaMailSender javaMailSender;
 
   public static void main(String[] args) {
     SpringApplication.run(ShipOrganizerServiceApplication.class, args);
