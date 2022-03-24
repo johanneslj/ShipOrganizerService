4455e994ed6d9d2282cc7137c45f2f16aaf5da8a
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-03-24 13:28:16 +0100

    Refactored some method implementations.

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/AuthController.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/AuthController.java
@@ -15,7 +15,6 @@ import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +61,7 @@ public class AuthController {
       System.out.println(email);
       System.out.println(password);
 
-      Optional<User> userOptional = loginService.login(email, password);
+      Optional<User> userOptional = loginService.loginAndGetUserOptional(email, password);
       if (userOptional.isPresent()) {
         User user = userOptional.get();
 
