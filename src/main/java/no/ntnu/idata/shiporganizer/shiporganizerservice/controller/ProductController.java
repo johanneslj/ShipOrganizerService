5affde174476d990986b22a1177bc7e00fe15262
Hans <hansal@ntnu.no>
2022-03-25 13:31:45 +0100

    Started work on creating new product

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductController.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductController.java
@@ -2,6 +2,7 @@ package no.ntnu.idata.shiporganizer.shiporganizerservice.controller;
 
 import static java.lang.Float.parseFloat;
 
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -25,90 +26,112 @@ import org.springframework.web.bind.annotation.RestController;
 @Transactional
 public class ProductController {
 
-  private final ProductService productService;
+    private final ProductService productService;
 
-  ProductController(ProductService productService) {
-    this.productService = productService;
-  }
-
-  /**
-   * Gets Initial inventory.
-   *
-   * @return the inventory
-   */
-  @PostMapping(path = "/get-inventory")
-  public List<Product> getInventory(HttpEntity<String> entity) {
-    List<Product> products = new ArrayList<>();
-    try {
-      JSONObject json = new JSONObject(entity.getBody());
-      String department = json.getString("department");
-      products = productService.getInitialProductInventory(department);
-    } catch (JSONException ignored) {
+    ProductController(ProductService productService) {
+        this.productService = productService;
     }
-    return products;
-  }
 
-  /**
-   * Gets updated inventory.
-   *
-   * @return the last updated inventory
-   */
-  @PostMapping(path = "/recently-updated-inventory")
-  public List<Product> getUpdatedInventory(HttpEntity<String> entity) {
-    List<Product> UpdatedProducts = new ArrayList<>();
-    try {
-      JSONObject json = new JSONObject(entity.getBody());
-      String department = json.getString("department");
-      Date date = new Date(json.getString("date"));
-      UpdatedProducts = productService.getUpdatedProductInventory(department, date);
-
-    } catch (JSONException ignored) {
+    /**
+     * Gets Initial inventory.
+     *
+     * @return the inventory
+     */
+    @PostMapping(path = "/get-inventory")
+    public List<Product> getInventory(HttpEntity<String> entity) {
+        List<Product> products = new ArrayList<>();
+        try {
+            JSONObject json = new JSONObject(entity.getBody());
+            String department = json.getString("department");
+            products = productService.getInitialProductInventory(department);
+        } catch (JSONException ignored) {
+        }
+        return products;
     }
-    return UpdatedProducts;
-  }
 
-  /**
-   * Gets preferred inventory.
-   *
-   * @return the preferred inventory
-   */
-  @PostMapping(path = "/get-recommended-inventory")
-  public List<Product> getRecommendedInventory(HttpEntity<String> entity) {
-    List<Product> products = new ArrayList<>();
-    try {
-      JSONObject json = new JSONObject(entity.getBody());
-      String department = json.getString("department");
-      products = productService.getProductRecommendedInventory(department);
-    } catch (JSONException ignored) {
+    /**
+     * Gets updated inventory.
+     *
+     * @return the last updated inventory
+     */
+    @PostMapping(path = "/recently-updated-inventory")
+    public List<Product> getUpdatedInventory(HttpEntity<String> entity) {
+        List<Product> UpdatedProducts = new ArrayList<>();
+        try {
+            JSONObject json = new JSONObject(entity.getBody());
+            String department = json.getString("department");
+            Date date = new Date(json.getString("date"));
+            UpdatedProducts = productService.getUpdatedProductInventory(department, date);
+
+        } catch (JSONException ignored) {
+        }
+        return UpdatedProducts;
     }
-    return products;
-  }
 
-  /**
-   * Set new stock
-   *
-   * @param entity the request body
-   * @return 200 OK or 204 No content
-   */
-  @PostMapping(path = "/set-new-stock")
-  public ResponseEntity<String> setNewStock(HttpEntity<String> entity) {
-    try {
-      JSONObject json = new JSONObject(entity.getBody());
-
-      setNewStockFromJson(json);
-    } catch (Exception e) {
-      e.printStackTrace();
+    /**
+     * Gets preferred inventory.
+     *
+     * @return the preferred inventory
+     */
+    @PostMapping(path = "/get-recommended-inventory")
+    public List<Product> getRecommendedInventory(HttpEntity<String> entity) {
+        List<Product> products = new ArrayList<>();
+        try {
+            JSONObject json = new JSONObject(entity.getBody());
+            String department = json.getString("department");
+            products = productService.getProductRecommendedInventory(department);
+        } catch (JSONException ignored) {
+        }
+        return products;
     }
-    return ResponseEntity.ok().build();
-  }
 
-  private void setNewStockFromJson(JSONObject json) throws JSONException {
-    productService.setNewStock(
-        json.optString("productnumber"),
-        json.getString("username"),
-        json.optInt("quantity"),
-        parseFloat(json.optString("longitude")),
-        parseFloat(json.optString("latitude")));
-  }
+    @PostMapping(path = "/new-product")
+    public ResponseEntity<String> createNewProduct(HttpEntity<String> entity) {
+        try {
+            JSONObject json = new JSONObject(entity.getBody());
+            String productName = json.getString("productName");
+            int productNumber = Integer.parseInt(json.getString("productNumber"));
+            int stock = Integer.parseInt(json.getString("stock"));
+            String barcode = json.getString("barcode");
+            String department = json.getString("department");
+
+            boolean success = productService.createNewProduct(productName, productNumber, stock, barcode, department);
+            if(success) {
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+
+        } catch (JSONException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    /**
+     * Set new stock
+     *
+     * @param entity the request body
+     * @return 200 OK or 204 No content
+     */
+    @PostMapping(path = "/set-new-stock")
+    public ResponseEntity<String> setNewStock(HttpEntity<String> entity) {
+        try {
+            JSONObject json = new JSONObject(entity.getBody());
+
+            setNewStockFromJson(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    private void setNewStockFromJson(JSONObject json) throws JSONException {
+        productService.setNewStock(
+            json.optString("productnumber"),
+            json.getString("username"),
+            json.optInt("quantity"),
+            parseFloat(json.optString("longitude")),
+            parseFloat(json.optString("latitude")));
+    }
 
 }
