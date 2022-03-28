bfeeb460eb3f77b97a53d6b03c5344cd310b3c94
Hans <hansal@ntnu.no>
2022-03-28 11:18:59 +0200

    Added edit product functionality, and wrote some javadoc

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/ProductService.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/ProductService.java
@@ -13,86 +13,88 @@ import java.util.List;
 @Service
 public class ProductService {
 
-	private final ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-	/**
-	 * Instantiates a new Product service.
-	 *
-	 * @param productRepository the product repository
-	 */
-	public ProductService(ProductRepository productRepository) {
-		this.productRepository = productRepository;
-	}
+    /**
+     * Instantiates a new Product service.
+     *
+     * @param productRepository the product repository
+     */
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
-	/**
-	 * Gets initial product inventory.
-	 *
-	 * @param department the users department
-	 * @return the product inventory
-	 */
-	public List<Product> getInitialProductInventory(String department) {
-		return productRepository.getInitialProductInventory(department);
-	}
+    /**
+     * Gets initial product inventory.
+     *
+     * @param department the users department
+     * @return the product inventory
+     */
+    public List<Product> getInitialProductInventory(String department) {
+        return productRepository.getInitialProductInventory(department);
+    }
 
-	/**
-	 * Gets updated product inventory.
-	 *
-	 * @param department the users department
-	 * @param date
-	 * @return the product inventory
-	 */
-	public List<Product> getUpdatedProductInventory(String department, String date) {
-		return productRepository.getUpdatedProductInventory(department,date);
-	}
-
-
-	/**
-	 * Gets product preferred inventory.
-	 *
-	 * @param department the users department
-	 * @return the product preferred inventory
-	 */
-	public List<Product> getProductRecommendedInventory(String department) {
-		return productRepository.getProductRecommendedInventory(department);
-	}
+    /**
+     * Gets updated product inventory.
+     *
+     * @param department the users department
+     * @param date
+     * @return the product inventory
+     */
+    public List<Product> getUpdatedProductInventory(String department, String date) {
+        return productRepository.getUpdatedProductInventory(department, date);
+    }
 
 
-	/**
-	 * Increases or decreases the stock for a specific product.
-	 *
-	 * @param productNo the product number
-	 * @param username  the username
-	 * @param quantity  the quantity
-	 * @param longitude the longitude
-	 * @param latitude  the latitude
-	 * @return Successful if update is successful
-	 */
-	public String setNewStock(String productNo,String username, int quantity, float longitude,float latitude, String date) {
-		int result = productRepository.setNewStock(productNo, username, quantity, longitude, latitude,date);
-		if(result == 1){
-			return "Success";
-		}
-		return "";
-	}
+    /**
+     * Gets product preferred inventory.
+     *
+     * @param department the users department
+     * @return the product preferred inventory
+     */
+    public List<Product> getProductRecommendedInventory(String department) {
+        return productRepository.getProductRecommendedInventory(department);
+    }
 
-	public boolean createNewProduct(String productName, int productNumber, int desiredStock, int stock, String barcode, String department) {
-		boolean success = false;
 
-		int successInt =  productRepository.createNewProduct(productName, productNumber, desiredStock, stock, barcode, department);
+    /**
+     * Increases or decreases the stock for a specific product.
+     *
+     * @param productNo the product number
+     * @param username  the username
+     * @param quantity  the quantity
+     * @param longitude the longitude
+     * @param latitude  the latitude
+     * @return Successful if update is successful
+     */
+    public String setNewStock(String productNo, String username, int quantity, float longitude, float latitude, String date) {
+        int result = productRepository.setNewStock(productNo, username, quantity, longitude, latitude, date);
+        if(result == 1) {
+            return "Success";
+        }
+        return "";
+    }
 
-		if(successInt == 1) {
-			success = true;
-		}
+    public boolean createNewProduct(String productName, int productNumber, int desiredStock, int stock, String barcode, String department) {
+        boolean success = false;
 
-		return success;
-	}
+        int successInt = productRepository.createNewProduct(productName, productNumber, desiredStock, stock, barcode, department);
 
-	public boolean editProduct(String productName, int productNumber, int desiredStock, String barcode, String department) {
-		boolean success = false;
+        if(successInt == 1) {
+            success = true;
+        }
 
-		//productRepository.editProduct() //TODO make repository update product details
+        return success;
+    }
 
-		return success;
-	}
+    public boolean editProduct(String productName, int productNumber, int desiredStock, String barcode, String department) {
+        boolean success = false;
+
+        int successInt = productRepository.editProduct(productName, productNumber, desiredStock, barcode, department);
+        if(successInt == 1) {
+            success = true;
+        }
+        return success;
+    }
 
 }
