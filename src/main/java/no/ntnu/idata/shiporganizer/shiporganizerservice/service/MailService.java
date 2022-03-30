d6df0e21933330215cd50688c1196acb1b3879d8
Simon Duggal <simondu@stud.ntnu.no>
2022-03-30 09:18:07 +0200

    Added create pfd and send pdf as mail
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/MailService.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/MailService.java
@@ -1,9 +1,16 @@
 package no.ntnu.idata.shiporganizer.shiporganizerservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class MailService {
@@ -45,4 +52,29 @@ public class MailService {
 
         javaMailSender.send(msg);
     }
+
+    /// Sends the created PDF for all the products to order as an Email to a specific email
+    public void sendPdfEmail(String userEmail, String[] recipients, String fileToAdd) throws MessagingException {
+        MimeMessage msg = javaMailSender.createMimeMessage();
+        try {
+            FileSystemResource file = new FileSystemResource(new File(fileToAdd));
+            MimeMessageHelper helper = new MimeMessageHelper(msg, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
+            helper.setTo(userEmail);
+            helper.setCc(recipients);
+            helper.setSubject("Ship Organizer: PDF for orders");
+            helper.setText("There has been a order list sent from the Ship Organizer app.\n" +
+                    "Please see attachment");
+            helper.addAttachment("Order.pdf", file);
+
+            javaMailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
+
+
 }
--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/ProductService.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/ProductService.java
@@ -1,11 +1,19 @@
 package no.ntnu.idata.shiporganizer.shiporganizerservice.service;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Product;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import javax.mail.MessagingException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * The Product service. Gets data from the Product repository modifies it and passes it to the controller
@@ -14,14 +22,18 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final MailService mailService;
+
 
     /**
      * Instantiates a new Product service.
      *
      * @param productRepository the product repository
+     * @param mailService
      */
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, MailService mailService) {
         this.productRepository = productRepository;
+        this.mailService = mailService;
     }
 
     /**
@@ -97,4 +109,70 @@ public class ProductService {
         return success;
     }
 
+    /// Creates the pdf with correct content and calls the mailService to send the email with the pdf
+    public void createPdf(List<Product> products, String email, String[] recipients){
+        try{
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream("BestillingsListe.pdf"));
+            document.open();
+            addMetaData(document);
+            addTextToPdf(document,email);
+            addTableToPdf(document,products);
+            document.close();
+
+            mailService.sendPdfEmail(email,recipients,"BestillingsListe.pdf");
+        }catch (DocumentException | FileNotFoundException | MessagingException e ){
+            System.out.println(e.getMessage());
+        }
+    }
+    // Add File meta data
+    private static void addMetaData(Document document) {
+        document.addTitle("Produkter for bestilling");
+        document.addAuthor("Ship Organizer app");
+        document.addCreator("Ship Organizer app");
+    }
+    // Add Text to pdf
+    private static void addTextToPdf(Document document,String email) throws DocumentException {
+        Paragraph preface = new Paragraph();
+        Font font = FontFactory.getFont(FontFactory.TIMES, 16, BaseColor.BLACK);
+        addEmptyLine(preface, 1);
+        preface.add(new Paragraph("Produkter for bestilling", font));
+        addEmptyLine(preface, 1);
+        preface.add(new Paragraph(
+                "Rapport generert av:",
+                font));
+        preface.add(new Paragraph(
+                "Email: " + email,
+                font));
+        preface.add(new Paragraph(
+                "Dato: " + new Date(),
+                font));
+        addEmptyLine(preface, 3);
+        document.add(preface);
+    }
+    // Add Table with products to pdf
+    private static void addTableToPdf(Document document, List<Product> products) throws DocumentException {
+        PdfPTable table = new PdfPTable(3);
+        Stream.of("Produktnavn", "Produktnummer", "Antall å bestille")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(header);
+                });
+        for (Product item : products) {
+            table.addCell(item.getProductName());
+            table.addCell(item.getProductNumber());
+            table.addCell(item.getStock());
+        }
+        document.add(table);
+    }
+    // Adds an empty line to the pdf
+    private static void addEmptyLine(Paragraph paragraph, int number) {
+        for (int i = 0; i < number; i++) {
+            paragraph.add(new Paragraph(" "));
+        }
+    }
 }
