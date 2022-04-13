fae2ed31932760144a234443ef6090387792b35b
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-04-13 18:46:40 +0200

    Trying to get tests to work.

---
+++ src/test/java/no/ntnu/idata/shiporganizer/shiporganizerservice/controller/ProductControllerTest.java
@@ -0,0 +1,12 @@
+package no.ntnu.idata.shiporganizer.shiporganizerservice.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductControllerTest {
+
+  @Test
+  void getInventory() {
+  }
+}
