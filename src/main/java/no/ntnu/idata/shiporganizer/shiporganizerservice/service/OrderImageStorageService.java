f1be1a93e494c2aae98d2e051701245a8ade5470
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-04-22 10:32:38 +0200

    Image functionality added.

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/OrderImageStorageService.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/service/OrderImageStorageService.java
@@ -6,33 +6,35 @@ import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.IOException;
-import java.util.List;
 import no.ntnu.idata.shiporganizer.shiporganizerservice.config.DoSpaceConfig;
-import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Image;
-import no.ntnu.idata.shiporganizer.shiporganizerservice.repository.ImageRepository;
+import no.ntnu.idata.shiporganizer.shiporganizerservice.model.Order;
+import no.ntnu.idata.shiporganizer.shiporganizerservice.repository.OrderRepository;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class ImageService {
-  private final String FOLDER_NAME = "shiporg/";
+public class OrderImageStorageService {
+  private final String FOLDER_NAME = "images/";
   private final DoSpaceConfig spaceConfig;
   private final AmazonS3 client;
-  private final ImageRepository imageRepository;
+  private final OrderRepository orderRepository;
 
-  public ImageService(
+  public OrderImageStorageService(
       DoSpaceConfig spaceConfig,
       AmazonS3 client,
-      ImageRepository imageRepository) {
+      OrderRepository orderRepository) {
     this.spaceConfig = spaceConfig;
     this.client = client;
-    this.imageRepository = imageRepository;
+    this.orderRepository = orderRepository;
   }
 
-  public void storeFile(MultipartFile multipartFile) throws IOException {
+  public void storeFile(MultipartFile multipartFile, int orderId) throws IOException {
+    if (multipartFile == null) {
+      throw new IOException("MultipartFile cannot be null.");
+    }
     uploadToServer(multipartFile);
-    saveImageToRepository(multipartFile);
+    linkImageToOrder(multipartFile, orderId);
   }
 
   private void uploadToServer(MultipartFile multipartFile) throws IOException {
@@ -58,32 +60,29 @@ public class ImageService {
   }
 
   private String getKeyFromMultipartFile(MultipartFile multipartFile) {
-    return FOLDER_NAME + FilenameUtils.removeExtension(multipartFile.getOriginalFilename()) + "." +
+    return FOLDER_NAME + getFileName(multipartFile);
+  }
+
+  static String getFileName(MultipartFile multipartFile) {
+    return FilenameUtils.removeExtension(multipartFile.getOriginalFilename()) + "." +
         FilenameUtils.getExtension(multipartFile.getOriginalFilename());
   }
 
-  private void saveImageToRepository(MultipartFile multipartFile) {
-    Image image = new Image(
-        FilenameUtils.removeExtension(multipartFile.getOriginalFilename()),
-        FilenameUtils.getExtension(multipartFile.getOriginalFilename())
-    );
-    imageRepository.save(image);
+  private void linkImageToOrder(MultipartFile multipartFile, int orderId) {
+    orderRepository.findById(orderId).ifPresent((order) -> {
+      order.setImageName(multipartFile.getOriginalFilename());
+      orderRepository.save(order);
+    });
   }
 
   public void deleteFile(int id) {
-    imageRepository
+    orderRepository
         .findById(id)
-        .ifPresent((image) -> {
-          client.deleteObject(
-              new DeleteObjectRequest(spaceConfig.getBucket(), getKeyFromImage(image)));
-        });
+        .ifPresent((order) -> client.deleteObject(
+            new DeleteObjectRequest(spaceConfig.getBucket(), getKeyFromOrder(order))));
   }
 
-  private String getKeyFromImage(Image image) {
-    return FOLDER_NAME + image.getName() + "." + image.getFileExtension();
-  }
-
-  public List<Image> getAllImages() {
-    return imageRepository.findAll();
+  private String getKeyFromOrder(Order order) {
+    return FOLDER_NAME + order.getImageName();
   }
 }
