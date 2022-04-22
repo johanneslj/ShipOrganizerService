64420e4c1035ade34c545a6c0057ed80931f218f
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-04-22 10:32:38 +0200

    Image functionality added.

--- src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/repository/ImageRepository.java
+++ src/main/java/no/ntnu/idata/shiporganizer/shiporganizerservice/repository/ImageRepository.java
@@ -8,5 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface ImageRepository extends CrudRepository<Image, Integer> {
   Optional<Image> findById(int id);
 
+  Image save(Image image);
+
   List<Image> findAll();
 }
