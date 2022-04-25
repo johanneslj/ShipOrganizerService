6b4ba04c965c9d4bcbfad5c845fd9b196be3862e
Simon Duggal <simondu@stud.ntnu.no>
2022-04-25 13:10:59 +0200

    Added the scripts to be executed on startup
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

---
+++ scripts/insert.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE  IF NOT EXISTS `KystFiske_db`;
+USE `KystFiske_db`;
+
+Insert into Department(DepartmentName, Rights) values ('Skipper',1);
+Insert into Department(DepartmentName, Rights) values ('Tråldekk',0);
+Insert into Department(DepartmentName, Rights) values ('Fabrikk',0);
+Insert into Department(DepartmentName, Rights) values ('Styrmann',0);
+Insert into Department(DepartmentName, Rights) values ('Bysse',0);
+Insert into Department(DepartmentName, Rights) values ('Maskin',0);
+Insert into Department(DepartmentName, Rights) values ('Kontor',1);
+
